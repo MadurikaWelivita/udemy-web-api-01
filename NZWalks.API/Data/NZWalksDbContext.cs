675afbd4a31d3ec98573499f21887b18923a060d
Chamika Sandamal Somasiri <madurikawelivita@gmail.com>
2024-01-31 21:30:11 -0500

    updated db context class

--- NZWalks.API/Data/NZWalksDbContext.cs
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
 
 namespace NZWalks.API.Data
 {
     public class NZWalksDbContext:DbContext
     {
-        
+        public NZWalksDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties {get; set;}
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+
     }
 }
